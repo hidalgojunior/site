efe012cf31b0592bb194110abfb69bed09bf04c7
Arnaldo Martins Hidalgo Junior <arnaldo@hidalgojunior.com.br>
2022-08-02 00:16:06 -0300

    Primeira versão do site

---
+++ index.php
@@ -0,0 +1,127 @@
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8" />
+    <link rel="stylesheet" href="estilo/all.min.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<meta name="keyword" content="palavras-chave,do,meu,site" />
+	<meta name="description" content="Descrição do Site" />
+    <link rel="stylesheet" href="estilo/style.css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300,400;0,500;1,400;1,500&display=swap" rel="stylesheet">
+	<title>HIDALGOJUNIOR - Site Oficial</title>
+</head>
+<body>
+	<header>
+        <div class="center">
+		<div class="logo left">HidalgoJunior</div><!-- logo -->
+        <nav class="desktop right">
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">Sobre</a></li>
+                <li><a href="">Serviços</a></li>
+                <li><a href="">Contato</a></li>
+            </ul>
+        </nav>
+        <nav class="mobile right">
+            <div class="botao-menu-mobile">
+                <i class="fa fa-bars" aria-hidden="true"></i>
+            </div>
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">Sobre</a></li>
+                <li><a href="">Serviços</a></li>
+                <li><a href="">Contato</a></li>
+            </ul>
+        </nav>
+            <div class="clear"></div>
+        </div><!-- center -->
+	</header>
+    <section class="banner-principal">
+        <div class="overlay"></div><!-- overlay -->
+        <div class="center">
+        <form>
+            <h2>Qual o seu melhor e-mail?</h2>
+            <input type="email" name="email" required/>
+            <input type="submit" name="acao" value="Cadastrar"/>
+        </form>
+        </div><!-- center -->
+    </section> <!-- banner-principal-->
+    <section class="descricao-autor">
+        <div class="center">
+        <div class="w50 left">
+            <h2>Arnaldo Martins Hidalgo Junior</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce semper efficitur scelerisque. Donec et felis sit amet nunc pretium imperdiet a id tortor. Sed id condimentum elit, a rutrum mauris. Mauris at volutpat nunc. Nulla dui mauris, vehicula fermentum ante eu, hendrerit fringilla purus. In tempus, turpis quis eleifend vulputate, ligula tortor feugiat libero, nec cursus ipsum velit id turpis. Aenean condimentum diam et ornare tristique. Donec sed mauris et neque interdum mollis. Donec erat justo, tempor at purus eu, suscipit molestie leo.
+            </p>
+            <p>
+                Pellentesque sed egestas justo. Nullam at velit orci. Cras vel dolor sit amet tellus efficitur fermentum id ac lorem. Fusce bibendum justo magna, eu accumsan dui vehicula sed. Aenean pulvinar nibh nisi, id sollicitudin augue rutrum vel. Suspendisse dictum leo eu risus interdum, in vestibulum nisl laoreet. Duis eleifend pulvinar mauris, ac luctus mauris pretium eu. Etiam rutrum est eu tellus faucibus, a dictum diam porttitor. Ut tristique leo nec blandit sollicitudin.
+            </p>
+
+        </div>
+        <div class="w50 left">
+            <img class="right" src="images/foto.png" />
+        </div>
+            <div class="clear"></div>
+        </div><!-- center -->
+    </section>
+    <section class="especialidades">
+        <div class="center ">
+           <h2 class="title">Especialidades</h2>
+            <div class="w33 left box-especialidades">
+                <h3><i class="fa-brands fa-css3"></i></h3>
+                <h3>CSS3</h3>
+                <p>Pellentesque sed egestas justo. Nullam at velit orci. Cras vel dolor sit amet tellus efficitur fermentum id ac lorem. Fusce bibendum justo magna, eu accumsan dui vehicula sed. Aenean pulvinar nibh nisi, id sollicitudin augue rutrum vel. Suspendisse dictum leo eu risus interdum, in vestibulum nisl laoreet. Duis eleifend pulvinar mauris, ac luctus mauris pretium eu. Etiam rutrum est eu tellus faucibus, a dictum diam porttitor. Ut tristique leo nec blandit sollicitudin.</p>
+            </div><!-- box-especialidades -->
+            <div class="w33 left box-especialidades">
+                <h3><i class="fa-brands fa-html5"></i></h3>
+                <h3>HTML5</h3>
+                <p>Pellentesque sed egestas justo. Nullam at velit orci. Cras vel dolor sit amet tellus efficitur fermentum id ac lorem. Fusce bibendum justo magna, eu accumsan dui vehicula sed. Aenean pulvinar nibh nisi, id sollicitudin augue rutrum vel. Suspendisse dictum leo eu risus interdum, in vestibulum nisl laoreet. Duis eleifend pulvinar mauris, ac luctus mauris pretium eu. Etiam rutrum est eu tellus faucibus, a dictum diam porttitor. Ut tristique leo nec blandit sollicitudin.</p>
+            </div><!-- box-especialidades -->
+            <div class="w33 left box-especialidades">
+                <h3><i class="fa-brands fa-php"></i></h3>
+                <h3>PHP</h3>
+                <p>Pellentesque sed egestas justo. Nullam at velit orci. Cras vel dolor sit amet tellus efficitur fermentum id ac lorem. Fusce bibendum justo magna, eu accumsan dui vehicula sed. Aenean pulvinar nibh nisi, id sollicitudin augue rutrum vel. Suspendisse dictum leo eu risus interdum, in vestibulum nisl laoreet. Duis eleifend pulvinar mauris, ac luctus mauris pretium eu. Etiam rutrum est eu tellus faucibus, a dictum diam porttitor. Ut tristique leo nec blandit sollicitudin.</p>
+            </div><!-- box-especialidades -->
+        </div><!-- center -->
+    </section>
+    <section class="extras">
+        <div class="center">
+            <div class="w50 left">
+                <h2>Depoimentos</h2>
+                <div class="depoimentos-single">
+                    <p class="depoimento-descricao">Pellentesque sed egestas justo. Nullam at velit orci. Cras vel dolor sit amet tellus efficitur fermentum id ac lorem. Fusce bibendum justo magna, eu accumsan dui vehicula sed. Aenean pulvinar nibh nisi, id sollicitudin augue rutrum vel. </p>
+                    <p class="depoimento-autor">Lorem Ipsum</p>
+                </div><!-- depoimentos-single-->
+                <div class="depoimentos-single">
+                    <p class="depoimento-descricao">Pellentesque sed egestas justo. Nullam at velit orci. Cras vel dolor sit amet tellus efficitur fermentum id ac lorem. Fusce bibendum justo magna, eu accumsan dui vehicula sed. Aenean pulvinar nibh nisi, id sollicitudin augue rutrum vel. </p>
+                    <p class="depoimento-autor">Lorem Ipsum</p>
+                </div><!-- depoimentos-single-->
+                <div class="depoimentos-single">
+                    <p class="depoimento-descricao">Pellentesque sed egestas justo. Nullam at velit orci. Cras vel dolor sit amet tellus efficitur fermentum id ac lorem. Fusce bibendum justo magna, eu accumsan dui vehicula sed. Aenean pulvinar nibh nisi, id sollicitudin augue rutrum vel. </p>
+                    <p class="depoimento-autor">Lorem Ipsum</p>
+                </div><!-- depoimentos-single-->
+            </div><!-- w50 -->
+            <div class="w50 left">
+                <h2>Serviços</h2>
+                <div class="servicos">
+                    <ul>
+                        <li>Pellentesque sed egestas justo. Nullam at velit orci. Cras vel dolor sit amet tellus efficitur fermentum id ac lorem. Fusce bibendum justo magna, eu accumsan dui vehicula sed. Aenean pulvinar nibh nisi, id sollicitudin augue rutrum vel.</li>
+                        <li>Pellentesque sed egestas justo. Nullam at velit orci. Cras vel dolor sit amet tellus efficitur fermentum id ac lorem. Fusce bibendum justo magna, eu accumsan dui vehicula sed. Aenean pulvinar nibh nisi, id sollicitudin augue rutrum vel.</li>
+                        <li>Pellentesque sed egestas justo. Nullam at velit orci. Cras vel dolor sit amet tellus efficitur fermentum id ac lorem. Fusce bibendum justo magna, eu accumsan dui vehicula sed. Aenean pulvinar nibh nisi, id sollicitudin augue rutrum vel.</li>
+                        <li>Pellentesque sed egestas justo. Nullam at velit orci. Cras vel dolor sit amet tellus efficitur fermentum id ac lorem. Fusce bibendum justo magna, eu accumsan dui vehicula sed. Aenean pulvinar nibh nisi, id sollicitudin augue rutrum vel.</li>
+                    </ul>
+                </div><!-- servicos -->
+            </div><!-- w50 -->
+            <div class="clear"></div>
+        </div> <!-- center -->
+    </section> <!-- Extras -->
+
+    <footer>
+        <div class="center">
+            <p>Todos os direitos reservados.</p>
+        </div>
+    </footer>
+</body>
+</html>
