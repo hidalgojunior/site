90d3d137bc9dbf72fa753ca15dbc046651d9a159
Arnaldo MartinsHidalgo Junior <arnaldo@hidalgojunior.com.br>
2022-08-03 20:31:27 -0300

    Alterando a pagina para Hombridade

--- index.php
+++ index.php
@@ -11,18 +11,18 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
- 	<title>HIDALGOJUNIOR - Site Oficial</title>
+ 	<title>HOMBRIDADE - Marília - SP</title>
 </head>
 <body>
 	<header>
         <div class="center">
-		<div class="logo left">HidalgoJunior</div><!-- logo -->
+		<div class="logo left">HOMBRIDADE</div><!-- logo -->
         <nav class="desktop right">
             <ul>
                 <li><a href="">Home</a></li>
-                <li><a href="">Sobre</a></li>
-                <li><a href="">Serviços</a></li>
-                <li><a href="">Contato</a></li>
+                <li><a href="">Módulos</a></li>
+                <li><a href="">Líderes</a></li>
+                <li><a href="">Fale Conosco</a></li>
             </ul>
         </nav>
         <nav class="mobile right">
@@ -31,9 +31,9 @@
             </div>
             <ul>
                 <li><a href="">Home</a></li>
-                <li><a href="">Sobre</a></li>
-                <li><a href="">Serviços</a></li>
-                <li><a href="">Contato</a></li>
+                <li><a href="">Módulos</a></li>
+                <li><a href="">Líderes</a></li>
+                <li><a href="">Fale Conosco</a></li>
             </ul>
         </nav>
             <div class="clear"></div>
@@ -43,7 +43,7 @@
         <div class="overlay"></div><!-- overlay -->
         <div class="center">
         <form>
-            <h2>Qual o seu melhor e-mail?</h2>
+            <h2>Qual o seu <strong>melhor</strong> e-mail?<br/>Cadastre-se e entraremos em contato.</h2>
             <input type="email" name="email" required/>
             <input type="submit" name="acao" value="Cadastrar"/>
         </form>
@@ -52,37 +52,38 @@
     <section class="descricao-autor">
         <div class="center">
         <div class="w50 left">
-            <h2>Arnaldo M. Hidalgo Jr.</h2>
+        <img class="left" src="images/logo.png" />    
+        
+        </div>
+        <div class="w50 left">
+        <h2>Arnaldo M. Hidalgo Jr.</h2>
             <h4>E-mail: <a href="mailto:arnaldo.hidalgo@etec.sp.gov.br">arnaldo.hidalgo@etec.sp.gov.br</a></h4>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce semper efficitur scelerisque. Donec et felis sit amet nunc pretium imperdiet a id tortor. Sed id condimentum elit, a rutrum mauris. Mauris at volutpat nunc. Nulla dui mauris, vehicula fermentum ante eu, hendrerit fringilla purus. In tempus, turpis quis eleifend vulputate, ligula tortor feugiat libero, nec cursus ipsum velit id turpis. Aenean condimentum diam et ornare tristique. Donec sed mauris et neque interdum mollis. Donec erat justo, tempor at purus eu, suscipit molestie leo.
             </p>
             <p>
                 Pellentesque sed egestas justo. Nullam at velit orci. Cras vel dolor sit amet tellus efficitur fermentum id ac lorem. Fusce bibendum justo magna, eu accumsan dui vehicula sed. Aenean pulvinar nibh nisi, id sollicitudin augue rutrum vel. Suspendisse dictum leo eu risus interdum, in vestibulum nisl laoreet. Duis eleifend pulvinar mauris, ac luctus mauris pretium eu. Etiam rutrum est eu tellus faucibus, a dictum diam porttitor. Ut tristique leo nec blandit sollicitudin.
             </p>
-
-        </div>
-        <div class="w50 left">
-            <img class="right" src="images/foto.png" />
+    
         </div>
             <div class="clear"></div>
         </div><!-- center -->
     </section>
     <section class="especialidades">
         <div class="center ">
-           <h2 class="title">Especialidades</h2>
+           <h2 class="title">Módulos</h2>
             <div class="w33 left box-especialidades">
-                <h3><i class="fa-brands fa-css3"></i></h3>
-                <h4>CSS3</h4>
+                <h3><i class="fa-solid fa-laptop-code"></i></h3>
+                <h4>Homem ao Máximo</h4>
                 <p>Pellentesque sed egestas justo. Nullam at velit orci. Cras vel dolor sit amet tellus efficitur fermentum id ac lorem. </p>
             </div><!-- box-especialidades -->
             <div class="w33 left box-especialidades">
                 <h3><i class="fa-brands fa-html5"></i></h3>
-                <h4>HTML5</h4>
+                <h4>Homem de Verdade</h4>
                 <p>Pellentesque sed egestas justo. Nullam at velit orci. Cras vel dolor sit amet tellus efficitur fermentum id ac lorem. </p>
             </div><!-- box-especialidades -->
             <div class="w33 left box-especialidades">
                 <h3><i class="fa-brands fa-php"></i></h3>
-                <h4>PHP</h4>
+                <h4>Tesouro</h4>
                 <p>Pellentesque sed egestas justo. Nullam at velit orci. Cras vel dolor sit amet tellus efficitur fermentum id ac lorem. </p>
             </div><!-- box-especialidades -->
             <div class="clear"></div>
