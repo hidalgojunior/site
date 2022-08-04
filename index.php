c9e87ed7d8b3373d28ca5b54c85ce245d752d6c3
Arnaldo Martins Hidalgo Junior <arnaldo@hidalgojunior.com.br>
2022-08-04 12:48:41 -0300

    Atualizando o site para Hombridade

--- index.php
+++ index.php
@@ -1,131 +1,14 @@
-
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="en">
 <head>
-	<meta charset="utf-8" />
-    <link rel="stylesheet" href="estilo/all.min.css">
-	<meta name="viewport" content="width=device-width, initial-scale=1" />
-	<meta name="keyword" content="palavras-chave,do,meu,site" />
-	<meta name="description" content="Descrição do Site" />
-    <link rel="stylesheet" href="estilo/style.css" />
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
- 	<title>HOMBRIDADE - Marília - SP</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="index.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>.:: Hombridade - Marília - SP ::. Site em Desenvolvimento .::</title>
 </head>
 <body>
-	<header>
-        <div class="center">
-		<div class="logo left">HOMBRIDADE</div><!-- logo -->
-        <nav class="desktop right">
-            <ul>
-                <li><a href="">Home</a></li>
-                <li><a href="">Módulos</a></li>
-                <li><a href="">Líderes</a></li>
-                <li><a href="">Fale Conosco</a></li>
-            </ul>
-        </nav>
-        <nav class="mobile right">
-            <div class="botao-menu-mobile">
-                <i class="fa fa-bars" aria-hidden="true"></i>
-            </div>
-            <ul>
-                <li><a href="">Home</a></li>
-                <li><a href="">Módulos</a></li>
-                <li><a href="">Líderes</a></li>
-                <li><a href="">Fale Conosco</a></li>
-            </ul>
-        </nav>
-            <div class="clear"></div>
-        </div><!-- center -->
-	</header>
-    <section class="banner-principal">
-        <div class="overlay"></div><!-- overlay -->
-        <div class="center">
-        <form>
-            <h2>Qual o seu <strong>melhor</strong> e-mail?<br/>Cadastre-se e entraremos em contato.</h2>
-            <input type="email" name="email" required/>
-            <input type="submit" name="acao" value="Cadastrar"/>
-        </form>
-        </div><!-- center -->
-    </section> <!-- banner-principal-->
-    <section class="descricao-autor">
-        <div class="center">
-        <div class="w50 left">
-        <img class="left" src="images/logo.png" />    
-        
-        </div>
-        <div class="w50 left">
-        <h2>Arnaldo M. Hidalgo Jr.</h2>
-            <h4>E-mail: <a href="mailto:arnaldo.hidalgo@etec.sp.gov.br">arnaldo.hidalgo@etec.sp.gov.br</a></h4>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce semper efficitur scelerisque. Donec et felis sit amet nunc pretium imperdiet a id tortor. Sed id condimentum elit, a rutrum mauris. Mauris at volutpat nunc. Nulla dui mauris, vehicula fermentum ante eu, hendrerit fringilla purus. In tempus, turpis quis eleifend vulputate, ligula tortor feugiat libero, nec cursus ipsum velit id turpis. Aenean condimentum diam et ornare tristique. Donec sed mauris et neque interdum mollis. Donec erat justo, tempor at purus eu, suscipit molestie leo.
-            </p>
-            <p>
-                Pellentesque sed egestas justo. Nullam at velit orci. Cras vel dolor sit amet tellus efficitur fermentum id ac lorem. Fusce bibendum justo magna, eu accumsan dui vehicula sed. Aenean pulvinar nibh nisi, id sollicitudin augue rutrum vel. Suspendisse dictum leo eu risus interdum, in vestibulum nisl laoreet. Duis eleifend pulvinar mauris, ac luctus mauris pretium eu. Etiam rutrum est eu tellus faucibus, a dictum diam porttitor. Ut tristique leo nec blandit sollicitudin.
-            </p>
-    
-        </div>
-            <div class="clear"></div>
-        </div><!-- center -->
-    </section>
-    <section class="especialidades">
-        <div class="center ">
-           <h2 class="title">Módulos</h2>
-            <div class="w33 left box-especialidades">
-                <h3><i class="fa-solid fa-laptop-code"></i></h3>
-                <h4>Homem ao Máximo</h4>
-                <p>Pellentesque sed egestas justo. Nullam at velit orci. Cras vel dolor sit amet tellus efficitur fermentum id ac lorem. </p>
-            </div><!-- box-especialidades -->
-            <div class="w33 left box-especialidades">
-                <h3><i class="fa-brands fa-html5"></i></h3>
-                <h4>Homem de Verdade</h4>
-                <p>Pellentesque sed egestas justo. Nullam at velit orci. Cras vel dolor sit amet tellus efficitur fermentum id ac lorem. </p>
-            </div><!-- box-especialidades -->
-            <div class="w33 left box-especialidades">
-                <h3><i class="fa-brands fa-php"></i></h3>
-                <h4>Tesouro</h4>
-                <p>Pellentesque sed egestas justo. Nullam at velit orci. Cras vel dolor sit amet tellus efficitur fermentum id ac lorem. </p>
-            </div><!-- box-especialidades -->
-            <div class="clear"></div>
-        </div><!-- center -->
-    </section>
-    <section class="extras">
-        <div class="center">
-            <div class="w50 left depoimentos-container">
-                <div class="title">Depoimento dos alunos e ex-alunos</div>
-                <h2>Depoimentos</h2>
-                <div class="depoimentos-single">
-                    <p class="depoimento-descricao">Pellentesque sed egestas justo. Nullam at velit orci. Cras vel dolor sit amet tellus efficitur fermentum id ac lorem. Fusce bibendum justo magna, eu accumsan dui vehicula sed. Aenean pulvinar nibh nisi, id sollicitudin augue rutrum vel. </p>
-                    <p class="depoimento-autor">Lorem Ipsum</p>
-                </div><!-- depoimentos-single-->
-                <div class="depoimentos-single">
-                    <p class="depoimento-descricao">Pellentesque sed egestas justo. Nullam at velit orci. Cras vel dolor sit amet tellus efficitur fermentum id ac lorem. Fusce bibendum justo magna, eu accumsan dui vehicula sed. Aenean pulvinar nibh nisi, id sollicitudin augue rutrum vel. </p>
-                    <p class="depoimento-autor">Lorem Ipsum</p>
-                </div><!-- depoimentos-single-->
-                <div class="depoimentos-single">
-                    <p class="depoimento-descricao">Pellentesque sed egestas justo. Nullam at velit orci. Cras vel dolor sit amet tellus efficitur fermentum id ac lorem. Fusce bibendum justo magna, eu accumsan dui vehicula sed. Aenean pulvinar nibh nisi, id sollicitudin augue rutrum vel. </p>
-                    <p class="depoimento-autor">Lorem Ipsum</p>
-                </div><!-- depoimentos-single-->
-            </div><!-- w50 -->
-            <div class="w50 left servicos-container">
-                <h2>Serviços</h2>
-                <div class="servicos">
-                    <ul>
-                        <li>Pellentesque sed egestas justo. Nullam at velit orci. Cras vel dolor sit amet tellus efficitur fermentum id ac lorem. Fusce bibendum justo magna, eu accumsan dui vehicula sed. Aenean pulvinar nibh nisi, id sollicitudin augue rutrum vel.</li>
-                        <li>Pellentesque sed egestas justo. Nullam at velit orci. Cras vel dolor sit amet tellus efficitur fermentum id ac lorem. Fusce bibendum justo magna, eu accumsan dui vehicula sed. Aenean pulvinar nibh nisi, id sollicitudin augue rutrum vel.</li>
-                        <li>Pellentesque sed egestas justo. Nullam at velit orci. Cras vel dolor sit amet tellus efficitur fermentum id ac lorem. Fusce bibendum justo magna, eu accumsan dui vehicula sed. Aenean pulvinar nibh nisi, id sollicitudin augue rutrum vel.</li>
-                        <li>Pellentesque sed egestas justo. Nullam at velit orci. Cras vel dolor sit amet tellus efficitur fermentum id ac lorem. Fusce bibendum justo magna, eu accumsan dui vehicula sed. Aenean pulvinar nibh nisi, id sollicitudin augue rutrum vel.</li>
-                    </ul>
-                </div><!-- servicos -->
-            </div><!-- w50 -->
-            <div class="clear"></div>
-        </div> <!-- center -->
-    </section> <!-- Extras -->
-
-    <footer>
-        <div class="center">
-            <p>Todos os direitos reservados.</p>
-        </div>
-    </footer>
+    <img src="images/logo.png" />
+    <p>Aguarde, site em desenvolvimento</p>
 </body>
 </html>
---
+++ index2.php
@@ -0,0 +1,131 @@
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8" />
+    <link rel="stylesheet" href="estilo/all.min.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<meta name="keyword" content="palavras-chave,do,meu,site" />
+	<meta name="description" content="Descrição do Site" />
+    <link rel="stylesheet" href="estilo/style.css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+ 	<title>HOMBRIDADE - Marília - SP</title>
+</head>
+<body>
+	<header>
+        <div class="center">
+		<div class="logo left">HOMBRIDADE</div><!-- logo -->
+        <nav class="desktop right">
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="#modulos">Módulos</a></li>
+                <li><a href="">Líderes</a></li>
+                <li><a href="">Fale Conosco</a></li>
+            </ul>
+        </nav>
+        <nav class="mobile right">
+            <div class="botao-menu-mobile">
+                <i class="fa fa-bars" aria-hidden="true"></i>
+            </div>
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="#modulos">Módulos</a></li>
+                <li><a href="">Líderes</a></li>
+                <li><a href="">Fale Conosco</a></li>
+            </ul>
+        </nav>
+            <div class="clear"></div>
+        </div><!-- center -->
+	</header>
+    <section class="banner-principal">
+        <div class="overlay"></div><!-- overlay -->
+        <div class="center">
+        <form>
+            <h2>Qual o seu <strong>melhor</strong> e-mail?<br/>Cadastre-se e entraremos em contato.</h2>
+            <input type="email" name="email" required/>
+            <input type="submit" name="acao" value="Cadastrar"/>
+        </form>
+        </div><!-- center -->
+    </section> <!-- banner-principal-->
+    <section class="descricao-autor">
+        <div class="center">
+        <div class="w50 left">
+        <img class="left" src="images/logo.png" />    
+        
+        </div>
+        <div class="w50 left">
+        <h2>Ministério de Homens - PIB Marília</h2>
+            <h4>E-mail: <a href="mailto:hombridade@hombridademarilia.com.br">hombridade@hombridademarilia.com.br</a></h4>
+        <p class="center">
+            (CMN - Christian Men's Network)</p>
+<p>A sociedade clama por homens maximizados por Deus. Homens íntegros e verdadeiramente semelhantes a Cristo, que desenvolvam todo o seu potencial, não desistam diante de dificuldade ou de falhas pessoais, saibam se comunicar com excelência, vivam em pureza sexual e sejam bons mordomos dos recursos dados por Deus.
+            </p>
+    
+        </div>
+            <div class="clear"></div>
+        </div><!-- center -->
+    </section>
+    <a name = "modulos">
+    <section class="especialidades">
+        <div class="center ">
+           <h2 class="title">Módulos</h2>
+            <div class="w33 left box-especialidades">
+                <h3><i class="fa-brands fa-bandcamp"></i></h3>
+                <h4>Homem ao Máximo</h4>
+                <p>Pellentesque sed egestas justo. Nullam at velit orci. Cras vel dolor sit amet tellus efficitur fermentum id ac lorem. </p>
+            </div><!-- box-especialidades -->
+            <div class="w33 left box-especialidades">
+                <h3><i class="fa-brands fa-bandcamp"></i></h3>
+                <h4>Homem de Verdade</h4>
+                <p>Pellentesque sed egestas justo. Nullam at velit orci. Cras vel dolor sit amet tellus efficitur fermentum id ac lorem. </p>
+            </div><!-- box-especialidades -->
+            <div class="w33 left box-especialidades">
+                <h3><i class="fa-brands fa-bandcamp"></i></h3>
+                <h4>Tesouro</h4>
+                <p>Pellentesque sed egestas justo. Nullam at velit orci. Cras vel dolor sit amet tellus efficitur fermentum id ac lorem. </p>
+            </div><!-- box-especialidades -->
+            <div class="clear"></div>
+        </div><!-- center -->
+    </section>
+    <section class="extras">
+        <div class="center">
+            <div class="w50 left depoimentos-container">
+                <div class="title">O que estão falando do Hombridade</div>
+                <h2>Testemunhos</h2>
+                <div class="depoimentos-single">
+                    <p class="depoimento-descricao">Pellentesque sed egestas justo. Nullam at velit orci. Cras vel dolor sit amet tellus efficitur fermentum id ac lorem. Fusce bibendum justo magna, eu accumsan dui vehicula sed. Aenean pulvinar nibh nisi, id sollicitudin augue rutrum vel. </p>
+                    <p class="depoimento-autor">Lorem Ipsum</p>
+                </div><!-- depoimentos-single-->
+                <div class="depoimentos-single">
+                    <p class="depoimento-descricao">Pellentesque sed egestas justo. Nullam at velit orci. Cras vel dolor sit amet tellus efficitur fermentum id ac lorem. Fusce bibendum justo magna, eu accumsan dui vehicula sed. Aenean pulvinar nibh nisi, id sollicitudin augue rutrum vel. </p>
+                    <p class="depoimento-autor">Lorem Ipsum</p>
+                </div><!-- depoimentos-single-->
+                <div class="depoimentos-single">
+                    <p class="depoimento-descricao">Pellentesque sed egestas justo. Nullam at velit orci. Cras vel dolor sit amet tellus efficitur fermentum id ac lorem. Fusce bibendum justo magna, eu accumsan dui vehicula sed. Aenean pulvinar nibh nisi, id sollicitudin augue rutrum vel. </p>
+                    <p class="depoimento-autor">Lorem Ipsum</p>
+                </div><!-- depoimentos-single-->
+            </div><!-- w50 -->
+            <div class="w50 left servicos-container">
+                <h2>Agenda e Eventos</h2>
+                <div class="servicos">
+                    <ul>
+                        <li>Pellentesque sed egestas justo. Nullam at velit orci. Cras vel dolor sit amet tellus efficitur fermentum id ac lorem. Fusce bibendum justo magna, eu accumsan dui vehicula sed. Aenean pulvinar nibh nisi, id sollicitudin augue rutrum vel.</li>
+                        <li>Pellentesque sed egestas justo. Nullam at velit orci. Cras vel dolor sit amet tellus efficitur fermentum id ac lorem. Fusce bibendum justo magna, eu accumsan dui vehicula sed. Aenean pulvinar nibh nisi, id sollicitudin augue rutrum vel.</li>
+                        <li>Pellentesque sed egestas justo. Nullam at velit orci. Cras vel dolor sit amet tellus efficitur fermentum id ac lorem. Fusce bibendum justo magna, eu accumsan dui vehicula sed. Aenean pulvinar nibh nisi, id sollicitudin augue rutrum vel.</li>
+                        <li>Pellentesque sed egestas justo. Nullam at velit orci. Cras vel dolor sit amet tellus efficitur fermentum id ac lorem. Fusce bibendum justo magna, eu accumsan dui vehicula sed. Aenean pulvinar nibh nisi, id sollicitudin augue rutrum vel.</li>
+                    </ul>
+                </div><!-- servicos -->
+            </div><!-- w50 -->
+            <div class="clear"></div>
+        </div> <!-- center -->
+    </section> <!-- Extras -->
+
+    <footer>
+        <div class="center">
+            <p>Todos os direitos reservados.</p>
+        </div>
+    </footer>
+</body>
+</html>
